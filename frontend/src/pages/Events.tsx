4d8b4a1231206fabd16ef84979e8c67bf2f40f4b
yunjia <yunjia.hao@mail.utoronto.ca>
2025-04-04 14:47:09 -0400

    fix route issue w/ events page

--- frontend/src/pages/Events.tsx
+++ frontend/src/pages/Events.tsx
@@ -155,12 +155,12 @@ export function Events() {
   };
 
   const handleEventClick = (eventId: number) => {
-    navigate(`${window.location.pathname}/events/${eventId}`);
+    navigate(`${window.location.pathname}/${eventId}`);
   };
 
   const handleCreateEvent = (eventId: number) => {
     setCreateDialogOpen(false);
-    navigate(`${window.location.pathname}/events/${eventId}`);
+    navigate(`${window.location.pathname}/${eventId}`);
   };
 
   return (
